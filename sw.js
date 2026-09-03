c8911ad115e26ab213341b0751cae8cb7709e88c
panckod91-lang <pancko.d91@gmail.com>
2026-09-03 19:44:44 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "d9-gestion-v0120-prod-clientes-pedidos";
+const CACHE = "d9-gestion-v0121-prod-inicio-movil";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./client-import.js", "./config.js", "./manifest.json", "./vendor/pdf.min.mjs", "./vendor/pdf.worker.min.mjs", "./icons/logo_d9.png", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
